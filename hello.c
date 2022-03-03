a64615e9f423c9216a7b940441fb9ce30d3870d5
rozhok <rozhkova2108@gmail.com>
2022-03-03 10:53:04 +0300

    izm

--- hello.c
+++ hello.c
@@ -5,6 +5,10 @@ int func(int a){
     int k=0, b, rez=0;
     for(k=0;a>0;k++){
         b=a%10;
+        if(b!=0 && b!=1){
+            rez='\0';
+            break;
+        }
         rez+= b*pow(2,k);
         a/=10;
     }
@@ -23,7 +27,13 @@ int main(){
     printf("ДВОИЧНАЯ\tДЕСЯТИЧНАЯ\n");
     for(i=0; i<n; ++i){
         printf("%8d\t", mas[i]);
-        printf("%8d\n", func(mas[i]));
+        n=func(mas[i]);
+        if(n=='\0'){
+            printf("ошибка\n");
+        }
+        else{
+             printf("%8d\n", n);
+        }
     }
     return 0;
 }
