72f3dbaa94568e92b7fd9f96c9fe3fa2f03e3399
rozhok <rozhkova2108@gmail.com>
2022-03-03 10:59:24 +0300

    izzzm

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int func(int a){
-    int k=0, b, rez=0;
+    int k=0, b=0, rez=0;
     for(k=0;a>0;k++){
         b=a%10;
         if(b!=0 && b!=1){
@@ -16,23 +16,22 @@ int func(int a){
 }
 
 int main(){
-    int i, n;
+    int i=0, n=0;
     printf("сколько хотите написать чисел:\n");
     scanf("%d", &n);
     int mas[n];
    printf("введите числа в двоичной системе:\n");
-   for (i=0; i<n; ++i){
+   for (i=0; i<n; i++){
        scanf("%d", &mas[i]);
    }
     printf("ДВОИЧНАЯ\tДЕСЯТИЧНАЯ\n");
-    for(i=0; i<n; ++i){
+    for(i=0; i<n; i++){
         printf("%8d\t", mas[i]);
-        n=func(mas[i]);
-        if(n=='\0'){
+        if(func(mas[i]) =='\0'){
             printf("ошибка\n");
         }
         else{
-             printf("%8d\n", n);
+            printf("%8d\n", func(mas[i]));
         }
     }
     return 0;
