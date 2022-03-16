68e5e7df59ffaf8fdd99e5491a7024ecaa749a01
rozhok <rozhkova2108@gmail.com>
2022-03-17 00:38:56 +0300

    dz3

--- hello.c
+++ hello.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
     FILE *matr, *graf;
     char m[100];
     char s = ' ';
-    int i = 0, a = 0;
+    int i = 0, a = 0, n=1;
     int k,j, rez1, kol;
     char rez2;
      matr = fopen("matr.txt", "r");
     while(!feof(matr)){
         if (a == 0 && s == '\n'){
-            a = i;
+            a = i; // количество ребер
+        }
+        if(s=='\n'){
+            n++;  //количество вершин
         }
         if (s == '1' || s == '0'){
             m[i] = s;
@@ -19,6 +23,13 @@ int main()
         }
         fscanf(matr, "%c", &s);
     }
+    if(a>((n-1)*(n-2)/2)){
+        printf("связный граф\n");
+    }
+    else{
+        printf("несвязный граф\n");
+    }
+    
     for ( j = 1; j <= a; j++){
         kol = 0;
         for ( k = j; k <= i; k++){
